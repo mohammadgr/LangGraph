d2d4fcb62637e31a9f4c3546227b3ae9b654ffef
Mohammad Geranmaieh <m.geranmaieh@aseh.local>
2025-10-27 13:42:55 +0330

    Develop sample LangGraph project, create model.ts, call compiled model in app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { result } from './dist/model.js';
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+for (const message of result.messages) {
+    console.log(`[${ message.type }]: ${ message.text }`);
+}
+
+
+app.get('/', (req, res) => {
+    res.send('Run model');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${ port }`);
+});
---
+++ modules/model.ts
@@ -0,0 +1,125 @@
+import { ChatOllama } from "@langchain/ollama";
+import { tool } from '@langchain/core/tools';
+import * as z from "zod";
+import { StateGraph, START, END } from "@langchain/langgraph";
+import { MessagesZodMeta } from "@langchain/langgraph";
+import { registry } from "@langchain/langgraph/zod";
+import { type BaseMessage } from "@langchain/core/messages";
+import { SystemMessage } from "@langchain/core/messages";
+import { AIMessage, ToolMessage } from "@langchain/core/messages";
+import { HumanMessage } from "@langchain/core/messages";
+
+const model = new ChatOllama({
+    model: "phi4-mini",
+    baseUrl: 'http://localhost:11434/',
+    temperature: 0,
+});
+
+const add = tool(({ a, b }) => a + b, {
+    name: "add",
+    description: "Add two numbers",
+    schema: z.object({
+        a: z.number().describe("First Number"),
+        b: z.number().describe("Second Number")
+    }),
+});
+
+const multiply = tool(({ a, b }) => a * b, {
+    name: "multiply",
+    description: "Multiply two numbers",
+    schema: z.object({
+        a: z.number().describe("First number"),
+        b: z.number().describe("Second number"),
+    }),
+});
+
+const divide = tool(({ a, b }) => a / b, {
+    name: "divide",
+    description: "Divide two numbers",
+    schema: z.object({
+        a: z.number().describe("First number"),
+        b: z.number().describe("Second number"),
+    }),
+});
+
+const minus = tool(({ a, b }) => a - b, {
+    name: "minus",
+    description: "Minus two numbers",
+    schema: z.object({
+        a: z.number().describe("First Number"),
+        b: z.number().describe("Second Number")
+    }),
+});
+
+const toolsByName = {
+    [add.name]: add,
+    [multiply.name]: multiply,
+    [divide.name]: divide,
+    [minus.name]: minus,
+};
+
+const tools = Object.values(toolsByName);
+const modelWithTools = model.bindTools(tools);
+
+const MessagesState = z.object({
+    messages: z
+        .array(z.custom<BaseMessage>())
+        .register(registry, MessagesZodMeta as any),
+    llmCalls: z.number().optional()
+});
+
+async function llmCall(state: z.infer<typeof MessagesState>) {
+    return {
+        messages: await modelWithTools.invoke([
+            new SystemMessage(
+                "You are a helpful assistant tasked with performing arithmetic on a set of inputs."
+            ),
+            ...state.messages,
+        ]),
+        llmCalls: (state.llmCalls ?? 0) + 1,
+    };
+}
+
+async function toolNode(state: z.infer<typeof MessagesState>) {
+    const lastMessage = state.messages.at(-1);
+
+    if (lastMessage == null || !AIMessage.isInstance(lastMessage)) {
+        return { messages: [] };
+    }
+
+    const result: ToolMessage[] = [];
+    for (const toolCall of lastMessage.tool_calls ?? []) {
+        const selectedTool = toolsByName[toolCall.name];
+        if (!selectedTool) {
+            throw new Error(`Tool "${toolCall.name}" not found`);
+        }
+        const observation = await selectedTool.invoke(toolCall);
+        result.push(observation);
+    }
+
+    return { messages: result };
+}
+
+async function shouldContinue(state: z.infer<typeof MessagesState>) {
+    const lastMessage = state.messages.at(-1);
+
+    if (lastMessage == null || !AIMessage.isInstance(lastMessage)) return END;
+
+    if (lastMessage.tool_calls?.length) {
+        return "toolNode";
+    }
+
+    return END;
+}
+
+const agent = new StateGraph(MessagesState)
+    .addNode("llmCall", llmCall)
+    .addNode("toolNode", toolNode)
+    .addEdge(START, 'llmCall')
+    .addConditionalEdges('llmCall', shouldContinue, ["toolNode", END])
+    .addEdge("toolNode", "llmCall")
+    .compile();
+
+export const result = await agent.invoke({
+    messages: [new HumanMessage("minus 56 and 8.")],
+});
